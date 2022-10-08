61262665f20c94bace3b216a22064c879b5d1030
fausberg <ivanfadeev2003@mail.ru>
2022-10-08 22:34:48 +0300

    homework_12

--- src/task2/Directory.java
+++ src/task2/Directory.java
@@ -9,11 +9,22 @@ public class Directory {
 
         for(int i = 0; i < dirlist.length; i++) {
             if(dirlist[i].isFile()){
-                System.out.println("Файл " + dirlist[i].getName());
+                System.out.println("Файл " + dirlist[i].getName() + " директории " + dir.getName());
+                if(dirlist[i].length() == 0){
+                    System.out.println("Файл пустой");
+                }else{
+                    System.out.println("В файле есть данные");
+                }
             }
             if(dirlist[i].isDirectory()) {
                 System.out.println("Директория " + dirlist[i].getName());
-                findFile(dirlist[i]);
+                File[] dirlist1 = dirlist[i].listFiles();
+                if(dirlist1.length == 0){
+                    System.out.println("Директория пуста");
+                }else {
+                    System.out.println("Файлы директории:");
+                    findFile(dirlist[i]);
+                }
             }
         }
     }
